4e215925d977399186d24a9cd83d125d8a4701e0
HieuLT24 <duchieuphung5@gmail.com>
2024-11-19 10:53:21 +0700

    login & register; pagination

--- saleapp/app/__init__.py
+++ saleapp/app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import quote
-
+import cloudinary
+from flask_login import LoginManager
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/saledb?charset=utf8mb4" % quote("Admin@123")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.secret_key='1978324yhdfjg183ysdfzxc??>m.bvk'
+app.config["PAGE_SIZE"] = 8
 
 db = SQLAlchemy(app)
+
+cloudinary.config(cloud_name='12adfgertjhe5',
+                  api_key='',
+                  api_secret='')
+
+login = LoginManager(app)
--- saleapp/app/models.py
+++ saleapp/app/models.py
@@ -1,6 +1,24 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from app import db, app
+from enum import Enum as RoleEnum
+import hashlib
+from flask_login import UserMixin
+
+class UserRole(RoleEnum):
+    ADMIN = 1
+    USER = 2
+
+class User(db.Model, UserMixin):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name= Column(String(255), nullable=False)
+    username = Column(String(100), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+    avatar = Column(String(100),
+                    default="https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg")
+    user_role = Column(Enum(UserRole), default=UserRole.USER)
+
 
 
 class Category(db.Model):
@@ -27,7 +45,11 @@ class Product(db.Model):
 
 if __name__ == '__main__':
     with app.app_context():
-        # db.create_all()
+        db.create_all()
+        u = User(name='admin', username='admin', password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()),user_role=UserRole.ADMIN)
+
+        db.session.add(u)
+        db.session.commit()
         # c1 = Category(name='Mobile')
         # c2 = Category(name='Tablet')
         # c3 = Category(name='Desktop')
@@ -35,58 +57,58 @@ if __name__ == '__main__':
         # db.session.add_all([c1, c2, c3])
         # db.session.commit()
 
-        products = [{
-            "name": "iPhone 7 Plus",
-            "description": "Apple, 32GB, RAM: 3GB, iOS13",
-            "price": 17000000,
-            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1691062682/tkeflqgroeil781yplxt.jpg",
-            "category_id": 1
-        }, {
-            "name": "iPad Pro 2020",
-            "description": "Apple, 128GB, RAM: 6GB",
-            "price": 37000000,
-            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690011099/uobyqhus0bhqt1qbh1qp.jpg",
-            "category_id": 2
-        }, {
-            "name": "Galaxy Note 10 Plus",
-            "description": "Samsung, 64GB, RAML: 6GB",
-            "price": 24000000,
-            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690011099/uobyqhus0bhqt1qbh1qp.jpg",
-            "category_id": 1
-        }, {
-            "name": "Galaxy Tab S9",
-            "description": "Apple, 128GB, RAM: 6GB",
-            "price": 37000000,
-            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1679134375/ckvdo90ltnfns77zf1xb.jpg",
-            "category_id": 2
-        }, {
-            "name": "iPhone 7 Plus",
-            "description": "Apple, 32GB, RAM: 3GB, iOS13",
-            "price": 17000000,
-            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1691062682/tkeflqgroeil781yplxt.jpg",
-            "category_id": 1
-        }, {
-            "name": "iPad Pro 2020",
-            "description": "Apple, 128GB, RAM: 6GB",
-            "price": 37000000,
-            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690011099/uobyqhus0bhqt1qbh1qp.jpg",
-            "category_id": 2
-        }, {
-            "name": "Galaxy Note 10 Plus",
-            "description": "Samsung, 64GB, RAML: 6GB",
-            "price": 24000000,
-            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690011099/uobyqhus0bhqt1qbh1qp.jpg",
-            "category_id": 1
-        }, {
-            "name": "Galaxy Tab S9",
-            "description": "Apple, 128GB, RAM: 6GB",
-            "price": 37000000,
-            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1679134375/ckvdo90ltnfns77zf1xb.jpg",
-            "category_id": 2
-        }]
-
-        for p in products:
-            p = Product(**p)
-            db.session.add(p)
-
-        db.session.commit()
+        # products = [{
+        #     "name": "iPhone 7 Plus",
+        #     "description": "Apple, 32GB, RAM: 3GB, iOS13",
+        #     "price": 17000000,
+        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1691062682/tkeflqgroeil781yplxt.jpg",
+        #     "category_id": 1
+        # }, {
+        #     "name": "iPad Pro 2020",
+        #     "description": "Apple, 128GB, RAM: 6GB",
+        #     "price": 37000000,
+        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690011099/uobyqhus0bhqt1qbh1qp.jpg",
+        #     "category_id": 2
+        # }, {
+        #     "name": "Galaxy Note 10 Plus",
+        #     "description": "Samsung, 64GB, RAML: 6GB",
+        #     "price": 24000000,
+        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690011099/uobyqhus0bhqt1qbh1qp.jpg",
+        #     "category_id": 1
+        # }, {
+        #     "name": "Galaxy Tab S9",
+        #     "description": "Apple, 128GB, RAM: 6GB",
+        #     "price": 37000000,
+        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1679134375/ckvdo90ltnfns77zf1xb.jpg",
+        #     "category_id": 2
+        # }, {
+        #     "name": "iPhone 7 Plus",
+        #     "description": "Apple, 32GB, RAM: 3GB, iOS13",
+        #     "price": 17000000,
+        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1691062682/tkeflqgroeil781yplxt.jpg",
+        #     "category_id": 1
+        # }, {
+        #     "name": "iPad Pro 2020",
+        #     "description": "Apple, 128GB, RAM: 6GB",
+        #     "price": 37000000,
+        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690011099/uobyqhus0bhqt1qbh1qp.jpg",
+        #     "category_id": 2
+        # }, {
+        #     "name": "Galaxy Note 10 Plus",
+        #     "description": "Samsung, 64GB, RAML: 6GB",
+        #     "price": 24000000,
+        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690011099/uobyqhus0bhqt1qbh1qp.jpg",
+        #     "category_id": 1
+        # }, {
+        #     "name": "Galaxy Tab S9",
+        #     "description": "Apple, 128GB, RAM: 6GB",
+        #     "price": 37000000,
+        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1679134375/ckvdo90ltnfns77zf1xb.jpg",
+        #     "category_id": 2
+        # }]
+        #
+        # for p in products:
+        #     p = Product(**p)
+        #     db.session.add(p)
+        #
+        # db.session.commit()
